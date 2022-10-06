c1df82d4aa22796f4893f04b82ea4cc5f4f831ef
Danny <49347714+itz-Malc0m@users.noreply.github.com>
2022-10-06 20:29:02 +0100

    This is the first commit to save the files

---
+++ src/adnek/LandPage.js
@@ -0,0 +1,391 @@
+import React from 'react';
+
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+// core components
+import AdNavBar from './AdNavBar';
+import LandPageHeader from './LandPageHeader';
+import DefaultFooter from 'components/Footers/DefaultFooter.js';
+
+function LandingPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add('landing-page');
+    document.body.classList.add('sidebar-collapse');
+    document.documentElement.classList.remove('nav-open');
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove('landing-page');
+      document.body.classList.remove('sidebar-collapse');
+    };
+  }, []);
+  return (
+    <>
+      <AdNavBar />
+      <div className="wrapper">
+        <LandPageHeader />
+        <div className="section section-about-us">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto text-center" md="8">
+                <h2 className="title">Who we are?</h2>
+                <h5 className="description">
+                  Adnek Food & Beverages is a company based in Abuja,Nigeria,
+                  dedicated to import and bringing the best and most premium
+                  products to our region and the rest of Africa, all selected by
+                  our experts that understand our clients needs and the latest
+                  trends...
+                </h5>
+              </Col>
+            </Row>
+            <div className="separator separator-primary"></div>
+            <div className="section-story-overview">
+              <Row>
+                <Col md="6">
+                  <div
+                    className="image-container image-left"
+                    style={{
+                      backgroundImage:
+                        'url(' + require('assets/img/3-Bottle.png') + ')',
+                    }}
+                  >
+                    <p className="blockquote blockquote-info">
+                      "We envision to become the most successful, respected and
+                      trusted patners in the region, offering the best service
+                      and brands to our clients" <br></br>
+                      <br></br>
+                      <small>-ADNEK_NG</small>
+                    </p>
+                  </div>
+                  <div
+                    className="image-container"
+                    style={{
+                      backgroundImage:
+                        'url(' +
+                        require('assets/img/PEARSE_Whiskey-Range.jpg') +
+                        ')',
+                    }}
+                  ></div>
+                </Col>
+                <Col md="5">
+                  <div
+                    className="image-container image-right"
+                    style={{
+                      backgroundImage:
+                        'url(' + require('assets/img/senor1.jpg') + ')',
+                    }}
+                  ></div>
+                  <h2 className="title">OUR CORE VALUES</h2>
+                  <h3>
+                    <span style={{ color: 'aqua', fontStyle: 'bold' }}>A</span>
+                    SSERTIVENESS
+                  </h3>
+                  <h3>
+                    <span style={{ color: 'aqua', fontStyle: 'bold' }}>D</span>
+                    EDICATION
+                  </h3>
+                  <h3>
+                    <span style={{ color: 'aqua', fontStyle: 'bold' }}>N</span>
+                    EAT
+                  </h3>
+                  <h3>
+                    <span style={{ color: 'aqua', fontStyle: 'bold' }}>E</span>
+                    XCELLENCE
+                  </h3>
+                  <h3>
+                    <span style={{ color: 'aqua', fontStyle: 'bold' }}>K</span>
+                    NOWLEDGE
+                  </h3>
+                  <h3>
+                    We are here to set the new record for the highest level of
+                    accolades for the premium services accross the region of
+                    Africa
+                  </h3>
+                  <p>
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens with climate change. Even if the
+                    Arctic continues to be one of the fastest-warming regions of
+                    the world, it will always be plunged into bitterly cold
+                    polar dark every winter. And year-by-year, for all kinds of
+                    natural reasons, there’s huge variety of the state of the
+                    ice.
+                  </p>
+                  <p>
+                    For a start, it does not automatically follow that a record
+                    amount of ice will melt this summer. More important for
+                    determining the size of the annual thaw is the state of the
+                    weather as the midnight sun approaches and temperatures
+                    rise. But over the more than 30 years of satellite records,
+                    scientists have observed a clear pattern of decline,
+                    decade-by-decade.
+                  </p>
+                  <p>
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens with climate change. Even if the
+                    Arctic continues to be one of the fastest-warming regions of
+                    the world, it will always be plunged into bitterly cold
+                    polar dark every winter. And year-by-year, for all kinds of
+                    natural reasons, there’s huge variety of the state of the
+                    ice.
+                  </p>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+        <div className="section section-team text-center">
+          <Container>
+            <h2 className="title">Here are our patners</h2>
+            <div className="team">
+              <Row>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require('assets/img/dublin.jpg')}
+                    ></img>
+                    <h4 className="title">Dublin City Spirits</h4>
+                    <p className="category text-info">Irish Liquor</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{' '}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{' '}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-instagram"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook-square"></i>
+                    </Button>
+                  </div>
+                </Col>
+
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require('assets/img/Pearse-logo-black-gold.png')}
+                    ></img>
+                    <h4 className="title">PEARSE</h4>
+                    <p className="category text-info">IRISH WHISKEY</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{' '}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{' '}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require('assets/img/nuvo2.jpg')}
+                    ></img>
+                    <h4 className="title">NUVO</h4>
+                    <p className="category text-info">Paris</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{' '}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{' '}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-google-plus"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-youtube"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="team-player">
+                    <img
+                      alt="..."
+                      className="rounded-circle img-fluid img-raised"
+                      src={require('assets/img/senor1.jpg')}
+                    ></img>
+                    <h4 className="title">SeNOR DE LOS CIALOS</h4>
+                    <p className="category text-info">MEXICAN TEQUILA</p>
+                    <p className="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some{' '}
+                      <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                        links
+                      </a>{' '}
+                      for people to be able to follow them outside the site.
+                    </p>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </Button>
+                    <Button
+                      className="btn-icon btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+        <div className="section section-contact-us text-center">
+          <Container>
+            <h2 className="title">Want to work with us?</h2>
+            <p className="description">Your project is very important to us.</p>
+            <Row>
+              <Col className="text-center ml-auto mr-auto" lg="6" md="8">
+                <InputGroup
+                  className={
+                    'input-lg' + (firstFocus ? ' input-group-focus' : '')
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons users_circle-08"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="First Name..."
+                    type="text"
+                    onFocus={() => setFirstFocus(true)}
+                    onBlur={() => setFirstFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <InputGroup
+                  className={
+                    'input-lg' + (lastFocus ? ' input-group-focus' : '')
+                  }
+                >
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="now-ui-icons ui-1_email-85"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email..."
+                    type="text"
+                    onFocus={() => setLastFocus(true)}
+                    onBlur={() => setLastFocus(false)}
+                  ></Input>
+                </InputGroup>
+                <div className="textarea-container">
+                  <Input
+                    cols="80"
+                    name="name"
+                    placeholder="Type a message..."
+                    rows="4"
+                    type="textarea"
+                  ></Input>
+                </div>
+                <div className="send-button">
+                  <Button
+                    block
+                    className="btn-round"
+                    color="info"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                    size="lg"
+                  >
+                    Send Message
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
